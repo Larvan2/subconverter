25d538ca7d1154328550c4c5ff30e6bf41a229eb
Larvan2 <78135608+Larvan2@users.noreply.github.com>
2023-03-23 01:10:04 +0800

    test

--- scripts/build.alpine.release.sh
+++ scripts/build.alpine.release.sh
@@ -8,19 +8,19 @@ git clone https://github.com/curl/curl
 cd curl
 git checkout curl-7_88_0
 cmake -DCURL_USE_MBEDTLS=ON -DHTTP_ONLY=ON -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=OFF -DCMAKE_USE_LIBSSH2=OFF -DBUILD_CURL_EXE=OFF . > /dev/null
-make install -j2 > /dev/null
+make install -j$(nproc) > /dev/null
 cd ..
 
 git clone https://github.com/jbeder/yaml-cpp --depth=1
 cd yaml-cpp
 cmake -DCMAKE_BUILD_TYPE=Release -DYAML_CPP_BUILD_TESTS=OFF -DYAML_CPP_BUILD_TOOLS=OFF . > /dev/null
-make install -j2 > /dev/null
+make install -j$(nproc) > /dev/null
 cd ..
 
 git clone https://github.com/ftk/quickjspp --depth=1
 cd quickjspp
 cmake -DCMAKE_BUILD_TYPE=Release .
-make quickjs -j2
+make quickjs -j$(nproc)
 install -m644 quickjs/libquickjs.a /usr/lib/
 install -d /usr/include/quickjs/
 install -m644 quickjs/quickjs.h quickjs/quickjs-libc.h /usr/include/quickjs/
@@ -31,7 +31,7 @@ git clone https://github.com/PerMalmberg/libcron --depth=1
 cd libcron
 git submodule update --init
 cmake -DCMAKE_BUILD_TYPE=Release .
-make libcron install -j2
+make libcron install -j$(nproc)
 cd ..
 
 git clone https://github.com/ToruNiina/toml11 --depth=1
@@ -42,7 +42,7 @@ cd ..
 
 export PKG_CONFIG_PATH=/usr/lib64/pkgconfig
 cmake -DCMAKE_BUILD_TYPE=Release .
-make -j2
+make -j$(nproc)
 rm subconverter
 g++ -o base/subconverter $(find CMakeFiles/subconverter.dir/src/ -name "*.o")  -static -lpcre2-8 -levent -lyaml-cpp -L/usr/lib64 -lcurl -lmbedtls -lmbedcrypto -lmbedx509 -lz -lquickjs -llibcron -O3 -s
 
